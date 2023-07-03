7abd896eb04797761a199287b0c2490ba1d2feb4
andre <andres@abarrientoso.com>
2023-07-02 22:13:33 -0600

    CRUD users, new registration and login features

--- Carwash/Controllers/UserController.cs
+++ Carwash/Controllers/UserController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net.Mail;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Carwash.Models;
+using Carwash.Models.ViewModel;
 
 namespace Carwash.Controllers
 {
@@ -21,17 +25,121 @@ namespace Carwash.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Registration()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Registration(User user)
+        public ActionResult Registration(users user, string confirmPassword)
         {
-            db.Users1.Add(user);
-            db.SaveChanges();
+
+            try
+            {
+                if (db.users.Any(u => u.email == user.email))
+                {
+                    ModelState.AddModelError("email", "Este email ya está registrado.");
+                    return View(user);
+                }
+                if (user.password != confirmPassword)
+                {
+                    ModelState.AddModelError(string.Empty, "Las contraseñas no coinciden.");
+                    return View(user);
+                }
+
+                string passwordPattern = @"^(?=.*[!@#$%^&*(),.?"":{}|<>])(?=.*[A-Z])(?=.*\d).+$";
+                if (!Regex.IsMatch(user.password, passwordPattern))
+                {
+                    ModelState.AddModelError("password", "La contraseña debe contener al menos un carácter especial, una mayúscula y un número.");
+                    return View(user);
+                }
+
+
+                string activationToken = GenerateActivationToken(user);
+                string hashedPassword = HashPassword(user.password.Trim().Replace(" ", ""));
+                user.email = user.email.Trim().Replace(" ", "");
+                user.isActive = false;
+                user.password = hashedPassword;
+                user.role_id = 3;
+                user.activation_token = activationToken;
+                db.users.Add(user);
+                db.SaveChanges();
+
+                TempData["ActivationEmail"] = user.email;
+                SendActivationMail(user.email, activationToken);
+
+                return RedirectToAction("ActivationSent");
+            }
+            catch (DbEntityValidationException ex)
+            {
+
+                foreach (var validationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        ModelState.AddModelError(validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+                return View(user);
+            }
+
+
+        }
+
+        public ActionResult ActivationSent()
+        {
+            string activationEmail = TempData["ActivationEmail"] as string;
+            ViewBag.ActivationEmail = activationEmail;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult ActivateAccount(string token)
+        {
+            ViewBag.Token = token;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ActivateAccount(ActivateAccountViewModel model, string token)
+        {
+
+            ActivateAccountViewModel tokenModel = new ActivateAccountViewModel();
+            tokenModel.Token = token;
+
+            if (ModelState.IsValid)
+            {
+                var user = db.users.FirstOrDefault(u => u.activation_token == token);
+
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Esta cuenta ya se encuentra activa o el token ha expirado");
+                    return View(model);
+                }
+
+                var comparePass = VerifyPassword(model.Password, user.password.Trim().Replace(" ", ""));
+
+                if ((user.email.Trim() == model.Email) && comparePass)
+                {
+                    user.isActive = true;
+                    user.activation_token = null;
+                    db.SaveChanges();
+
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Correo o contraseña incorrectos.");
+                }
+            }
+
+            ViewBag.Token = token;
+            return View(model);
         }
 
         public ActionResult Login()
@@ -40,17 +148,55 @@ namespace Carwash.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(User user)
+        public ActionResult Login(users user)
         {
-            var lg = db.Users1.Where(a => a.email.Equals(user.email) && a.password.Equals(user.password)).FirstOrDefault();
-            if (lg != null)
+            if (ModelState.IsValid)
             {
-                return RedirectToAction("Index", "User");
+                var loginUser = db.users.SingleOrDefault(u => u.email.Trim() == user.email);
+
+                if (loginUser != null)
+                {
+                    bool isActive = loginUser.isActive;
+
+                    if (!isActive)
+                    {
+                        ModelState.AddModelError("", "Esta cuenta no se encuentra activa. Regístrese o revise su correo para activarla.");
+                        return View(user);
+                    }
+
+                    // Verificar la contraseña utilizando una función hash segura
+                    bool isPasswordValid = VerifyPassword(user.password, loginUser.password);
+
+                    if (isPasswordValid)
+                    {
+                        if (loginUser.role_id == 1)
+                        {
+                            Session["Rol"] = "Administrador";
+                        } 
+                        else
+                        {
+                            Session["Rol"] = "Cliente";
+                        }
+                        return RedirectToAction("Index", "User");
+                    }
+                } 
+                else
+                {
+                    ModelState.AddModelError("", "El usuario no existe.");
+                    return View();
+                }
+
+                ModelState.AddModelError("", "Credenciales incorrectas.");
+                return View(user);
             }
-            else
-            {
-                return RedirectToAction("Login", "User");
-            }
+
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "User");
         }
 
         public ActionResult ForgotPassword()
@@ -70,7 +216,7 @@ namespace Carwash.Controllers
                     ViewBag.Error = "El token ha expirado";
                     return View("StartRecovery");
                 }
-                var oUser = db.Users1.Where(d => d.token_recovery == model.token).FirstOrDefault();
+                var oUser = db.users.Where(d => d.token_recovery == model.token).FirstOrDefault();
                 if (oUser == null)
                 {
                     ViewBag.Error = "El token ha expirado";
@@ -93,12 +239,17 @@ namespace Carwash.Controllers
 
                 using (Models.carwashEntities db = new Models.carwashEntities())
                 {
-                    var oUser = db.Users1.Where(d => d.token_recovery == model.token).FirstOrDefault();
+                    var oUser = db.users.Where(d => d.token_recovery == model.token).FirstOrDefault();
+                    string passwordPattern = @"^(?=.*[!@#$%^&*(),.?"":{}|<>])(?=.*[A-Z])(?=.*\d).+$";
+                    if (!Regex.IsMatch(model.Password, passwordPattern))
+                    {
+                        ModelState.AddModelError("", "La contraseña debe contener al menos un carácter especial, una mayúscula y un número.");
+                        return View("StartRecovery", model);
+                    }
                     if (oUser != null)
                     {
-                        oUser.password = GetSha256(model.Password);
+                        oUser.password = HashPassword(model.Password.Trim().Replace(" ", ""));
                         oUser.token_recovery = null;
-                        db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
                         db.SaveChanges();
                     }
                 }
@@ -109,7 +260,7 @@ namespace Carwash.Controllers
             }
 
             ViewBag.Message = "Contraseña modificada con exito";
-            return View("Index");
+            return RedirectToAction("Login");
         }
 
         [HttpGet]
@@ -133,7 +284,7 @@ namespace Carwash.Controllers
 
                 using (carwashEntities db = new carwashEntities())
                 {
-                    var oUser = db.Users1.Where(d => d.email == model.Email).FirstOrDefault();
+                    var oUser = db.users.Where(d => d.email == model.Email).FirstOrDefault();
                     if (oUser != null)
                     {
                         oUser.token_recovery = token;
@@ -153,52 +304,125 @@ namespace Carwash.Controllers
             }
         }
 
+        
         public ActionResult UsersList()
         {
-            List<User> users = db.Users1.ToList(); // Obtener todos los usuarios de la tabla
+            string rol = Session["Rol"] as string;
 
-            return View(users);
+            if (rol.Equals("Administrador"))
+            {
+                List<users> users = db.users.ToList(); // Obtener todos los usuarios de la tabla
+
+                return View(users);
+            }
+            else {
+                return RedirectToAction("Login", "User");
+            }
+
+            
 
         }
 
         public ActionResult UpdateAll(string[] lista)
         {
-            var users = db.Users1.ToList(); // Obtener todos los usuarios de la base de datos
-            int contador = 0;
-            foreach (var user in users)
+            string rol = Session["Rol"] as string;
+
+            if (rol.Equals("Administrador"))
             {
-                /*string selectedRole = Request.Form[user.userId.ToString()]; // Obtener el valor seleccionado del formulario
-
-                // Actualizar el valor de id_rol según el valor seleccionado
-                if (selectedRole == "Administrador")
+                var users = db.users.ToList(); // Obtener todos los usuarios de la base de datos
+                int contador = 0;
+                foreach (var user in users)
                 {
-                    user.id_rol = 1;
+
+                    user.role_id = int.Parse(lista[contador]);
+                    contador++;
+
+                    db.users.Attach(user);
+
+
+                    db.Entry(user).State = EntityState.Modified;
+
+                    db.SaveChanges();
                 }
-                else if (selectedRole == "Cliente")
-                {
-                    user.id_rol = 2;
-                }
-                else if (selectedRole == "Empleado")
-                {
-                    user.id_rol = 3;
-                }*/
-                user.id_rol = int.Parse(lista[contador]);
-                contador++;
 
-                db.Users1.Attach(user);
-              
 
-                db.Entry(user).State = EntityState.Modified;
-
-                db.SaveChanges();
+                return RedirectToAction("UsersList");
             }
-
-
-            return RedirectToAction("UsersList");
+            else {
+                return RedirectToAction("Login", "User");
+            }
+                
         }
 
+   
+        public ActionResult Delete(int id)
+        {
+            string rol = Session["Rol"] as string;
+
+            if (rol.Equals("Administrador"))
+            {
+                var lg = db.users.Where(a => a.user_id.Equals(id)).FirstOrDefault();
+
+                db.users.Remove(lg);
+                db.SaveChanges();
+                return RedirectToAction("UsersList");
+            }
+            else {
+                return RedirectToAction("Login", "User");
+            }
+
+                
+        }
 
         #region HELPERS
+        private string GenerateActivationToken(users user)
+        {
+            string token = Guid.NewGuid().ToString();
+            return token;
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+
+            byte[] hashBytes = Convert.FromBase64String(storedHash);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(enteredPassword, salt, 10000);
+            byte[] enteredHash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != enteredHash[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private string HashPassword(string password)
+        {
+
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+
+
+            byte[] hash = pbkdf2.GetBytes(20);
+
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+
+            return hashedPassword;
+        }
         private string GetSha256(string str)
         {
             SHA256 sha256 = SHA256Managed.Create();
@@ -212,12 +436,35 @@ namespace Carwash.Controllers
 
         private void SendEmail(string toAddress, string token)
         {
-            string fromAddress = "correo@correo.com";
-            string Contrasena = "su contraseña";
+            string fromAddress = "";
+            string Contrasena = "";
             string url = urlDomain + "/User/StartRecovery/?token=" + token;
             string subject = "Recuperación de contraseña";
             string body = "<p>Correo para recuperación de contraseña</p><br>" +
                 "<a href='" + url + "'>Click para recuperar</a>";
+
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(fromAddress, Contrasena);
+
+            MailMessage message = new MailMessage(fromAddress, toAddress, subject, body);
+            message.IsBodyHtml = true;
+
+            client.Send(message);
+
+            client.Dispose();
+        }
+
+        private void SendActivationMail(string toAddress, string activationToken)
+        {
+            string fromAddress = "";
+            string Contrasena = "";
+            string url = urlDomain + "/User/ActivateAccount/?token=" + activationToken;
+            string subject = "Activación de cuenta - Carwash";
+            string body = "<p>Hola! Gracias por registrarte en nuestro sitio web, para activar tu cuenta por favor ingresa a este link:</p><br>" +
+                "<a href='" + url + "'>Click aquí para activar su cuenta</a>";
 
 
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
---
+++ Carwash/Models/ViewModel/ActivateAccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carwash.Models.ViewModel
+{
+        public class ActivateAccountViewModel
+        {
+            public string Token { get; set; }
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+}
